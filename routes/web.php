7ef0e2ca1a21e15202f840a378e16cfe21730547
RoY3rS04 <rogermoises2004@gmail.com>
2026-03-12 15:42:34 -0600

    Job chains and job batches

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,30 @@ Route::get('/', function () {
 //        \App\Jobs\SendEmail::dispatch();
 //    }
 
-    \App\Jobs\SendEmail::dispatch();
+//    \App\Jobs\SendEmail::dispatch();
 
 //    \App\Jobs\ProcessPayment::dispatch()
 //        ->onQueue('payments');
+
+    // Dispatching a workflow
+//    $chain = [
+//        new \App\Jobs\PullRepo(),
+//        new \App\Jobs\RunTests(),
+//        new \App\Jobs\Deploy()
+//    ];
+
+    $batch = [
+        new \App\Jobs\PullRepo('laracasts/project1'),
+        new \App\Jobs\PullRepo('laracasts/project2'),
+        new \App\Jobs\PullRepo('laracasts/project3')
+    ];
+
+    //Bus::chain($chain)->dispatch();
+
+    // Processes the jobs in parallel
+    \Illuminate\Support\Facades\Bus::batch($batch)
+        ->allowFailures()
+        ->dispatch();
 
     return view('welcome');
 });

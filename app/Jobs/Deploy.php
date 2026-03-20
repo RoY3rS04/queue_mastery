79ce54f42767c5b8f2cea71a9249c5cf0eaf6aa6
RoY3rS04 <rogermoises2004@gmail.com>
2026-03-20 11:15:02 -0600

    Preventing race conditions in jobs

--- app/Jobs/Deploy.php
+++ app/Jobs/Deploy.php
@@ -4,6 +4,9 @@ namespace App\Jobs;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
 
 class Deploy implements ShouldQueue
 {
@@ -22,6 +25,46 @@ class Deploy implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        info('Started deploying...');
+
+        sleep(5);
+
+        info('Finished deploying!');
+
+        //Using Redis funnels
+        Redis::funnel('deployments')
+            ->limit(5)
+            ->block(10)
+            ->then(function () {
+                info('Started deploying...');
+
+                sleep(5);
+
+                info('Finished deploying!');
+            });
+
+        // 10 Deploy jobs every 60 seconds
+        Redis::throttle('deployments')
+            ->allow(10)
+            ->every(60)
+            ->then(function () {
+
+            });
+
+        // Using Locks
+//        Cache::lock('deployments')
+//            ->block(10, function () {
+//                info('Started deploying...');
+//
+//                sleep(5);
+//
+//                info('Finished deploying!');
+//            });
+    }
+
+    public function middleware() {
+        return [
+            new WithoutOverlapping('deployments', 10)
+        ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,18 +20,44 @@ Route::get('/', function () {
 //        new \App\Jobs\Deploy()
 //    ];
 
-    $batch = [
-        new \App\Jobs\PullRepo('laracasts/project1'),
-        new \App\Jobs\PullRepo('laracasts/project2'),
-        new \App\Jobs\PullRepo('laracasts/project3')
-    ];
+    // A batch of chains
+//    $batch = [
+//        [
+//            new \App\Jobs\PullRepo('laracasts/project1'),
+//            new \App\Jobs\RunTests('laracasts/project1'),
+//            new \App\Jobs\Deploy('laracasts/project1')
+//        ],
+//        [
+//            new \App\Jobs\PullRepo('laracasts/project2'),
+//            new \App\Jobs\RunTests('laracasts/project2'),
+//            new \App\Jobs\Deploy('laracasts/project2')
+//        ]
+//    ];
 
-    //Bus::chain($chain)->dispatch();
+    \App\Jobs\Deploy::dispatch();
+
+//    Bus::chain([
+//        new \App\Jobs\Deploy(),
+//        function () {
+//            \Illuminate\Support\Facades\Bus::batch([...])->dispatch();
+//        }
+//    ])->dispatch();
 
     // Processes the jobs in parallel
-    \Illuminate\Support\Facades\Bus::batch($batch)
-        ->allowFailures()
-        ->dispatch();
+//    \Illuminate\Support\Facades\Bus::batch($batch)
+//        ->catch(function ($batch, $e) {
+//
+//        })
+//        ->then(function ($batch) {
+//
+//        })
+//        ->finally(function ($batch) {
+//
+//        })
+//        ->onQueue('deployments')
+////        ->onConnection('database')
+//        ->allowFailures()
+//        ->dispatch();
 
     return view('welcome');
 });
